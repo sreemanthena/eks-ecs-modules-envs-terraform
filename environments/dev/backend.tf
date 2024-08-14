42771d711c750ff1f9318ae8b071b915abf829a3
Sreenivasa Manthena <sreemanthen.docs@gmail.com>
2024-08-14 08:00:37 +0530

    Updated code

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "sree-ecs-tfstate"
-    key    = "ecs/env/${terraform.workspace}/terraform.tfstate"
+    bucket = "sree-terraform"
+    key    = "dev-eks/terraform.tfstate"
     region = "us-east-1" 
   }
 }
